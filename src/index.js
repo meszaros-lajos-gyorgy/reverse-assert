3df431543182808eca067951f0e4dbd3489b2fc5
Lajos Meszaros <lajos.meszaros@performgroup.com>
2017-06-27 15:18:25 +0200

    feat(testWithType): Added function, which can use the information from datas

--- src/index.js
+++ src/index.js
@@ -5,7 +5,25 @@ const nonZeroInt = require('./datas/nonZeroInt')
 const nonZeroPositiveInt = require('./datas/nonZeroPositiveInt')
 const boolWithNullArray = require('./datas/boolWithNullArray')
 
+const assert = require('assert')
+
+const {
+  compose,
+  forEach,
+  toPairs
+} = require('ramda')
+
+const testWithType = it => (f, type, expected) => compose(
+  forEach(([name, value]) => {
+    it(`returns ${expected}, when given parameter is ${name}`, () => {
+      assert.equal(f(value), expected)
+    })
+  }),
+  toPairs
+)(type)
+
 module.exports = {
+  testWithType,
   plainObject,
   boolWithNull,
   int,
